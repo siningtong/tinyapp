703e645e69242a66185450bfa1da3ac0d3e0cb58
siningtong <siningtong@gmail.com>
2019-09-03 00:20:49 +0000

    pass urlDatabast to index

--- express_server.js
+++ express_server.js
@@ -19,6 +19,13 @@ app.get('/urls.json',(req,res)=>{
 
 app.get('/hello',(req,res)=>{
   res.send('<html><body>hello <b>World<b></body></html>\n')
+});
+
+app.get('/urls',(req,res)=>{
+  let templateVars = {
+    urls: urlDatabase
+  }
+  res.render('urls_index', {urls: urlDatabase})
 })
 
 app.listen(PORT,()=>{
